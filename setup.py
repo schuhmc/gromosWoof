1672932e5cab1b4922bb2fd652297b524cec4299
Marc Schuh <schuh.marc@students.boku.ac.at>
2023-03-28 16:18:59 +0200

    add url

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ setuptools.setup(
     version="0.0.1",
     description="A package to track the progress of running gromos simulations on a slurm cluster",
     author="Marc Schuh",
+    url="https://github.com/schuhmc/gromosWoof",
     packages=['gromosWoof'],
     install_requires=["setuptools>=61.0", "paramiko", "pandas"],
     classifiers=[
